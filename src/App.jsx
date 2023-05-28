0d5efec82ab0267f6449e3eb3b8a661794c052f8
Arishemm <arishem51@gmail.com>
2023-05-28 09:43:07 +0700

    feat: core concepts react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Grand" animal="Cat" breed="Spicy" />
+      <SearchParams />
     </div>
   );
 };
